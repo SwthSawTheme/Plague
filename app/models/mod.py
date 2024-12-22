f0fe4a55f75443419fbeab648865c2fd3334e83d
SwthSawTheme <sawtheme1@gmail.com>
2024-12-22 08:31:13 -0300

    Adicionado label de texto, titulo e botão a janela

--- app/models/mod.py
+++ app/models/mod.py
@@ -1,13 +1,22 @@
-import customtkinter
+import customtkinter as ctk
 
-class Plague(customtkinter.CTk):
+class Plague(ctk.CTk):
 
     def __init__(self):
         super().__init__()
 
         # JANELA
-        self.geometry("640x360")
-        self.title("Plague Hack | by Swth")
+        self.geometry("640x360") # Tamanho da janela
+        self.title("Plague Hack | by Swth") # Título da janela
         self._set_appearance_mode("dark") # Tema da janela
         self.resizable(False,False)   # Travar janela
 
+        # Widgets
+        self.label_instrucoes = ctk.CTkLabel(self, text="P L A G U E",font=("Arial",14))
+        self.label_instrucoes.pack(pady=10)
+
+        self.entrada = ctk.CTkEntry(self, placeholder_text="Quantidade DNA")
+        self.entrada.pack(pady=10)
+
+        self.botao_enviar = ctk.CTkButton(self, text="Inject", )
+        self.botao_enviar.pack(pady=10)
